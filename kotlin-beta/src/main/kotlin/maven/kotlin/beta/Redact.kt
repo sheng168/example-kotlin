3a2c3b45e2d864751f4f03c5db922b092e92eccf
Jin Yu <jin.yu@clearme.com>
2019-02-27 21:45:58 -0500

    wrapper class that doesn't show in json

--- kotlin-beta/src/main/kotlin/maven/kotlin/beta/Redact.kt
+++ kotlin-beta/src/main/kotlin/maven/kotlin/beta/Redact.kt
@@ -1,13 +1,23 @@
 package maven.kotlin.beta
 
+import com.fasterxml.jackson.annotation.JsonValue
+
 data class Redact(
-        val pii: String
+        val pii: String = ""
 ) {
     override fun toString(): String {
-        return super.toString().replace(pii, "<redacted>")
+        return "<redacted>"
     }
 
+    @JsonValue
     fun toJson(): String {
         return pii
+    }
+
+    companion object {
+        @JvmStatic
+        fun fromString(s: String): Redact {
+            return Redact(s)
+        }
     }
 }
